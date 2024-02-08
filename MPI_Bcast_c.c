be913cb9b008ff572c09bd30403571561f777216
David Henty <d.henty@epcc.ed.ac.uk>
2024-02-08 11:54:09 +0000

    Updated ./MPI_Bcast_c.c

---
+++ MPI_Bcast_c.c
@@ -0,0 +1,21 @@
+#include <mpi.h>
+
+int MPI_Bcast_c(void *buffer, MPI_Count count, MPI_Datatype datatype,
+                int root, MPI_Comm comm)
+{
+  MPI_Count finishc, subcountc, ic;
+
+  int subcounti;
+
+  static int chunki = 0x40000000; // 2^31
+
+  for (ic=0; ic < count; ic += chunki)
+    {
+      finishc = ic + chunki;
+      if (finishc > count) finishc = count;
+      subcountc = finishc - ic;
+      subcounti = subcountc;
+      MPI_Bcast(buffer+ic, subcounti, datatype, root, comm);
+    }
+}
+
